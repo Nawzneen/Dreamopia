312e42420c859b4f9d9dcaed14d8059dc417fa76
nawzneen <earworm90@gmail.com>
2024-06-24 15:47:12 +0300

    Feat: add provider

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,15 +1,11 @@
-'use client'
-import React from 'react'
-import {SessionProvider} from 'next-auth/react'
-import {Session} from 'next-auth'
+"use client";
+import React from "react";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 interface ProviderProps {
-    children: React.ReactNode;
-    session: Session | null | undefined; 
+  children: React.ReactNode;
+  session: Session | null | undefined;
 }
-export default function Provider({children, session}: ProviderProps) {
-  return (
-<SessionProvider session={session}>
-    {children}
-</SessionProvider>
-  )
+export default function Provider({ children, session }: ProviderProps) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
