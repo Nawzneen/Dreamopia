ac223851ddc2784c40497e130a1c1e90dc54ae58
nawzneen <earworm90@gmail.com>
2024-06-24 15:38:36 +0300

    Bug: fix username and callback

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,43 +9,50 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    try {
-      const db = await connectToDB();
-      const queryResult = await db.query(
-        "SELECT * FROM users WHERE email = $1",
-        [session.user.email]
-      );
-      const user = queryResult.rows[0];
-      // store user ID in session
-      session.user.id = user.id.toString();
-      return session;
-    } catch (error) {
-      console.error("error fetching user from postgreSQL", error.message);
-      throw new Error("erro fetching user from postgres");
-    }
-  },
-  async signIn({ profile }) {
-    try {
-      const db = await connectToDB();
-      const queryResult = await db.query(
-        "SELECT * FROM users WHERE email = $1",
-        [profile.email]
-      );
-      const user = queryResult.rows[0];
-      if (!user) {
-        const username = profile.username.replace(" ", "").toLowerCase();
-        const image = profile.picture || null;
-        await this.query(
-          "INSERT INTO users (username, email, image, created_at) VALUES ($1, $2, $3, NOW())",
-          [username, profile.email, profile.image]
+  callbacks: {
+    async session({ session }) {
+      try {
+        const db = await connectToDB();
+        const queryResult = await db.query(
+          "SELECT * FROM users WHERE email = $1",
+          [session.user.email]
         );
+        const user = queryResult.rows[0];
+        // store user ID in session
+        session.user.id = user.id.toString();
+        return session;
+      } catch (error) {
+        console.error("error fetching user from postgreSQL", error.message);
+        throw new Error("erro fetching user from postgres");
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      throw new Error("Error checking or creating user in PostgreSQL");
-    }
+    },
+    async signIn({ profile }) {
+        console.log("profile is",profile)
+      try {
+        const db = await connectToDB();
+        const queryResult = await db.query(
+          "SELECT * FROM users WHERE email = $1",
+          [profile.email]
+        );
+        console.log("query results isssssssssssss",queryResult)
+        const user = queryResult.rows[0];
+        console.log("user is", user)
+        if (!user) {
+            console.log("user doesnt exist")
+
+          const username = profile.name.replace(" ", "").toLowerCase();
+          const image = profile.picture || null;
+          await db.query(
+            "INSERT INTO users (username, email, image, created_at) VALUES ($1, $2, $3, NOW())",
+            [username, profile.email, image]
+          );
+        }
+        return true;
+      } catch (error) {
+        console.log(error.message);
+        throw new Error("Error checking or creating user in PostgreSQL");
+      }
+    },
   },
 });
 export { handler as GET, handler as POST };
