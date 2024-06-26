e43c6f1d0796e88d51ea2bdb66c84bd0a69fb78d
nawzneen <earworm90@gmail.com>
2024-06-26 14:48:31 +0300

    Feat: add Feed component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
-
+import Feed from "@components/Feed";
 export default function Home() {
   return (
-<section className="w-full flex-center flex-col">
-  <h1 className="text-center">Write what you think about
-    <br className="max-md:hidden"/>
-    <span className="text-center">Fantasy Fans</span>
-  </h1>
-  <p className="text-center description">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, architecto animi accusantium expedita quidem dolor magnam quasi ad laboriosam inventore amet porro, vitae tempore hic fugiat dicta tenetur et atque!
-  </p>
-</section>
+    <section className="w-full flex-center flex-col">
+      <h1 className="text-center">
+        Write what you think about
+        <br className="max-md:hidden" />
+        <span className="text-center">Fantasy Fans</span>
+      </h1>
+      <p className="text-center description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt,
+        architecto animi accusantium expedita quidem dolor magnam quasi ad
+        laboriosam inventore amet porro, vitae tempore hic fugiat dicta tenetur
+        et atque!
+      </p>
+      <Feed />
+    </section>
   );
 }
