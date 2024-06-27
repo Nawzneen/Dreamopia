f1fb0e6f2a93e0ba222f9e726e162e0fd4273ddb
nawzneen <earworm90@gmail.com>
2024-06-27 14:14:29 +0300

    Feat: Creat GET route for Users

---
+++ app/api/users/route.js
@@ -0,0 +1,15 @@
+import { connectToDB } from "@utils/database";
+export const GET = async (req, { params }) => {
+  // params for dynamic variables
+  try {
+    const db = await connectToDB();
+    const data = await db.query(
+      "SELECT * FROM users ORDER BY created_at DESC LIMIT 10 "
+    );
+    const users = data.rows;
+    console.log(users)
+    return new Response(JSON.stringify(users), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch a user posts", { status: 500 });
+  }
+};
