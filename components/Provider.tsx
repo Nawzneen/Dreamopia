cd4a729881caef5090c58eb14597ff494c6c4414
nawzneen <earworm90@gmail.com>
2024-07-02 13:18:41 +0300

    Feat: add types to provider

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -2,10 +2,11 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+// import {Session as SesssionType} from "../types/types"
 interface ProviderProps {
   children: React.ReactNode;
-  session: Session | null | undefined;
+  session?: Session | null ;
 }
-export default function Provider({ children, session }: ProviderProps) {
+export default function Provider({ children, session }: ProviderProps)  {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
